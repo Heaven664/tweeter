0bce0f0e5c464048183a81fcd9720f66e0ae7cba
Omar Hamid <omarhamid664@gmail.com>
2023-03-29 16:59:24 -0600

    implement loadTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,29 +1,13 @@
 $(() => {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+
+  /**
+   * Makes get request to /tweets using AJAX
+   */
+  const loadTweets = () => {
+    $.ajax({
+      url: '/tweets'
+    }).then((tweets) => renderTweets(tweets));
+  };
 
   /**
    * Renders a list of tweets
@@ -67,7 +51,7 @@ $(() => {
     return $tweet;
   };
 
-  renderTweets(data);
+  loadTweets();
 
   $('#new-tweet-from').submit((event) => {
     event.preventDefault();
@@ -75,7 +59,7 @@ $(() => {
       type: "POST",
       url: "/tweets",
       data: $('#new-tweet-from').serialize()
-    })
-  })
+    });
+  });
 })
 
